fc7f20fbcf1cce30d4a4859bd22b54d3533a5cb9
shangoman <abdulbaruwa@hotmail.com>
2014-09-05 05:32:27 +0100

    First insert into board

---
+++ KrossWordBuilder/KrossWordBuilder.Tests/BoardTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KrossWordBuilder.Tests
+{
+    [TestClass]
+    public class BoardTests
+    {
+        [TestMethod]
+        public void InitiatedBoard()
+        {
+            var board = new Board(12);
+            Assert.IsNotNull(board.Grids);
+            Assert.AreEqual(board.Grids.Length, 144);
+        }
+
+        [TestMethod]
+        public void AddNewWordToBoard()
+        {
+            var board = new Board(12);
+            board.AddWord("First");
+            Assert.AreEqual(board.Grids[0, 0], "F");
+            Assert.AreEqual(board.Grids[0, 1], "i");
+            Assert.AreEqual(board.Grids[0, 2], "r");
+            Assert.AreEqual(board.Grids[0, 3], "s");
+            Assert.AreEqual(board.Grids[0, 4], "t");
+        }
+
+        [TestMethod]
+        public void AddASecondWordVerticalIfFirstLetterMatches()
+        {
+            var board = new Board(12);
+            board.AddWord("first");
+            board.AddWord("restore");
+
+            Assert.AreEqual("r", board.Grids[0, 2]);
+            Assert.AreEqual("e", board.Grids[1, 2]);
+            Assert.AreEqual("s", board.Grids[2, 2]);
+            Assert.AreEqual("t", board.Grids[3, 2]);
+            Assert.AreEqual("o", board.Grids[4, 2]);
+            Assert.AreEqual("r", board.Grids[5, 2]);
+            Assert.AreEqual("e", board.Grids[6, 2]);
+        }
+
+        [TestMethod]
+        public void AddWordVerticalShouldFailIfThereIsCharInSuffixCell()
+        {
+            var board = new Board(12);
+            board.Grids[7, 2] = "x";
+            board.AddWord("first");
+            var wordAdded = board.AddWord("restore");
+            Assert.IsFalse(wordAdded);
+        }
+
+    }
+}
